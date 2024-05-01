089ef81c9d9f7e62fe9e001caa152c52d586872f
unknown <davidodojunior@gmail.com>
2024-05-01 11:35:44 -0700

    split_function_practise program

---
+++ split_function_practise.py
@@ -0,0 +1,20 @@
+#collect multiple inputs
+#use split function to separate them
+#Find even and odd numbers from user input
+
+items = list(map(int, input("Enter multpile values on this line: ").split()))
+
+for num in items:
+	if num % 2 == 0:
+
+		print(f"{num} is an even number")
+
+	else:
+
+		print(f"{num} is an odd number")
+		
+
+#print("Values are: ", items)
+
+#print("There are ", len(items), "items collected from user input")
+
