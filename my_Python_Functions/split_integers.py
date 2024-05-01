3b5527ea56d2fa1233535af812efd22501ada7b5
unknown <davidodojunior@gmail.com>
2024-05-01 11:30:56 -0700

    my_Python_Functions

---
+++ my_Python_Functions/split_integers.py
@@ -0,0 +1,9 @@
+def split_integers(num):
+    num = input("Enter a number: ")
+    temp = str(user_input)
+    for index in temp:
+        remainder = user_input % 10
+        user_input = user_input // 10
+        #print(str(remainder), end=" ")
+        
+    return str(remainder), end=" "
